69162c1d12f360ebb4351a6054de264e8c45c58e
TorGames <tor.blikra@gmail.com>
2026-01-12 22:21:31 +0100

    Light blinking added, from "esp32 getting started with ESP-IDF

--- src/main.c
+++ src/main.c
@@ -1,1 +1,22 @@
-void app_main() {}
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define LED_GPIO GPIO_NUM_10
+
+void app_main(void)
+{
+    uint8_t led_state = 0;
+
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        led_state = !led_state;
+
+        gpio_set_level(LED_GPIO, led_state);
+        printf("Led State %d\n", led_state);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+    }
+}
